fd7abe7f7d952c4d5cc978f414cf3f3517e08aed
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2024-03-19 21:29:55 +0100

    Finished screening and basic movie.css

--- Movie.cs
+++ Movie.cs
@@ -1,6 +1,22 @@
 public class Movie
 {
-    public string MovieName;
+    public readonly int ID;
+    public string Title;
     public int AgeRating;
-    public 
+    public List<int> ScreeningIDs;
+    public Movie(string title, int ageRating)
+    {
+        List<Movie> allMovies = JsonHandler.Read<Movie>("MovieDB.json");
+        MovieID = allMovies.Count + 1;
+        Title = title;
+        AgeRating = ageRating;
+        Screenings = new List<Screening>();
+    }
+
+    public bool AddScreening(Auditorium assignedAuditorium, DateTime screeningDateTime)
+    {
+        Screenings newScreening = new Screening(assignedAuditorium, screeningDateTime, this.ID);
+        Screenings.Add(newScreening);
+        JsonHandler.Update<Screening>(newScreening, "ScreeningDB.json");
+    }
 }
--- Screening.cs
+++ Screening.cs
@@ -1,27 +1,68 @@
 public class Screening
 {
+    public readonly int ID;
     public Auditorium AssignedAuditorium;
     public DateTime ScreeningDateTime;
-    public Screening(Auditorium assignedAuditorium, DateTime screeningDateTime)
+    public readonly int MovieID;
+    public Screening(Auditorium assignedAuditorium, string screeningDateTime, int movieID)
     {
+        List<Screening> allScreenings = JsonHandler.Read<Screening>("ScreeningDB.json");
+        ID = allScreenings.Count + 1;
         AssignedAuditorium = assignedAuditorium;
-        ScreeningDateTime = screeningDateTime;
+        ScreeningDateTime = DateTime.ParseExact(timeStampString, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+        MovieID = movieID;
     }
 
-    public bool AdjustDateTime(DateTime dateTime)
+    public bool AdjustDateTime(string dateTime)
     {
-        // Code to adjust date and time
-        return true;
+        try
+        {
+            ScreeningDateTime = DateTime.ParseExact(timeStampString, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+            bool result = JsonHandler.Update(this, "ScreeningsDB.Json");
+            return result;
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Format error. Please use [dd-MM-yyyy HH:mm]");
+            return false;
+        }
     }
 
-    public bool AdjustDateTime(DateTime time)
+    public bool AdjustTime(string time)
     {
-        //code to adjust solly time
-        return true;
+        try
+        {
+            TimeSpan newTime = TimeSpan.Parse(time);
+            ScreeningDateTime = ScreeningDateTime.Date + newTime;
+            bool result = JsonHandler.Update(this, "ScreeningsDB.Json");
+            return result;
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Format error. Please use [HH:mm]");
+            return false;
+        }
     }
-    public bool AdjustDateTime(DateTime date)
+
+    public bool AdjustDate(string date)
     {
-        //code to adjust solly date
-        return true;
+        try
+        {
+            ScreeningDateTime = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture) + ScreeningDateTime.TimeOfDay;
+            bool result = JsonHandler.Update(this, "ScreeningsDB.Json");
+            return result;
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Format error. Please use [dd-MM-yyyy]");
+            return false;
+        }
+    }
+
+    public bool AdjustAuditorium(Auditorium newAuditorium)
+    {
+        AssignedAuditorium = newAuditorium;
+        bool result = JsonHandler.Update(this, "ScreeningsDB.Json");
+        return result;
     }
 }
