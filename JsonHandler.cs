183f3d1b3224d3e678bfd4d4f72b7e27f80141de
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2024-03-19 14:11:25 +0100

    Finished basic jsonhandler

--- JsonHandler.cs
+++ JsonHandler.cs
@@ -1,6 +1,63 @@
+using Newtonsoft.Json;
+
 public static class JsonHandler
 {
-    public static bool Write(List<var> dataToWrite, string jsonFile)
+    public static bool Write<T>(List<T> dataToWrite, string jsonFile)
     {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(jsonFile))
+            {
+                string dataToWrite = JsonConvert.SerializeObject<List<T>>(dataToWrite);
+                writer.Write(dataToWrite);
+            }
+        }
+        catch (JsonWriterException ex)
+        {
+            Console.WriteLine($"Error reading JSON: {ex.Message}");
+            return false;
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"JSON file not found: {ex.Message}");
+            return false;
+        }
+
+        return true;
     }
+
+    public static bool Append<T>(T objectToAppend, string jsonFile)
+    {
+        List<T> listOfObjects = Read<T>(jsonFile);
+        listOfObjects.Add(objectToAppend);
+        Write<T>(listOfObjects);
+        return true;
+    }
+
+    public static List<T>? Read<T>(string jsonFile)
+    {
+        List<T> listOfObject = new List<T>();
+        try
+        {
+            using (StreamReader reader = new StreamReader(jsonFile))
+            {
+                string fileData = reader.ReadToEnd();
+                listOfObjects = JsonConvert.DeserializeObject<List<T>>(fileData);
+            }
+        }
+        catch (JsonReaderException ex)
+        {
+            Console.WriteLine($"Error reading JSON: {ex.Message}");
+            return null;
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"JSON file not found: {ex.Message}");
+            return null;
+        }
+
+        return listOfObjects;
+    }
+
+
 }
--- Screening.cs
+++ Screening.cs
@@ -1,5 +1,27 @@
 public class Screening
 {
     public Auditorium AssignedAuditorium;
-    public 
+    public DateTime ScreeningDateTime;
+    public Screening(Auditorium assignedAuditorium, DateTime screeningDateTime)
+    {
+        AssignedAuditorium = assignedAuditorium;
+        ScreeningDateTime = screeningDateTime;
+    }
+
+    public bool AdjustDateTime(DateTime dateTime)
+    {
+        // Code to adjust date and time
+        return true;
+    }
+
+    public bool AdjustDateTime(DateTime time)
+    {
+        //code to adjust solly time
+        return true;
+    }
+    public bool AdjustDateTime(DateTime date)
+    {
+        //code to adjust solly date
+        return true;
+    }
 }
