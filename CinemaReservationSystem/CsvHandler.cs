cf83f09e9f7626dfdd5a3e8fd3c21126f37cf930
HolyCowOfDoom <ut.ozyurt@gmail.com>
2024-03-19 01:12:00 +0100

    added notes

--- CinemaReservationSystem/CsvHandler.cs
+++ CinemaReservationSystem/CsvHandler.cs
@@ -1,8 +1,9 @@
 using CsvHelper;
 public static class CsvHandler{
     static string UserDBFilePath = "../UserDB.csv";
-    private static List<User> Users; // we could avoid using this list altogether using static methods in User.cs
-    
+    private static List<User> Users; //we could avoid using this list altogether by using static methods in User.cs together with methods below (I already tried, it does make things more complicated)
+    //but this list essentially doubles the size of our db so it might be worth it? 
+    //(we could manipulate the db directly with user input in static methods in User.cs rather than through a User obj)
     public static bool LoadUsers(){
         //Users =   ;
         return true;
