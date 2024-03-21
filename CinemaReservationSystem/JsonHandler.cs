a1a7e736d9e00fc6799a4fcc607660f5b20ea970
dabboloosefun <juliankreugel@gmail.com>
2024-03-21 23:17:26 +0100

    JsonHandler is now fully functional : ObjectHadID

--- CinemaReservationSystem/JsonHandler.cs
+++ CinemaReservationSystem/JsonHandler.cs
@@ -1,6 +1,11 @@
 using Newtonsoft.Json;
 using System.Linq;
 
+public interface ObjectHasID
+{
+    int ID { get; }
+}
+
 public static class JsonHandler
 {
     public static bool Write<T>(List<T> dataToWrite, string jsonFile)
@@ -9,8 +14,8 @@ public static class JsonHandler
         {
             using (StreamWriter writer = new StreamWriter(jsonFile))
             {
-                string dataToWrite = JsonConvert.SerializeObject<List<T>>(dataToWrite);
-                writer.Write(dataToWrite);
+                string stringToWrite = JsonConvert.SerializeObject(dataToWrite);
+                writer.Write(stringToWrite);
             }
         }
         catch (JsonWriterException ex)
@@ -29,21 +34,23 @@ public static class JsonHandler
 
     public static bool Append<T>(T objectToAppend, string jsonFile)
     {
-        List<T> listOfObjects = Read<T>(jsonFile);
+        List<T>? listOfObjects = Read<T>(jsonFile);
+
         if (listOfObjects == null) listOfObjects = new List<T>();
+        
         listOfObjects.Add(objectToAppend);
-        bool writeSucces = Write<T>(listOfObjects);
+        bool writeSucces = Write<T>(listOfObjects, jsonFile);
 
         return writeSucces;
     }
 
-    public static bool Update<T>(T objectToUpdate, string jsonFile)
+    public static bool Update<T>(T objectToUpdate, string jsonFile) where T : ObjectHasID
     {
         List<T>? listOfObjects = Read<T>(jsonFile);
         bool updated = false;
 
-        T objectInList = Get<T>(objectToUpdate.ID, jsonFile);
-        if (objectInList != null)
+        T? objectInList = Get<T>(objectToUpdate.ID, jsonFile);
+        if (objectInList != null && listOfObjects != null)
         {
             listOfObjects[listOfObjects.IndexOf(objectInList)] = objectToUpdate;
             updated = Write<T>(listOfObjects, jsonFile);
@@ -58,7 +65,7 @@ public static class JsonHandler
 
     public static List<T>? Read<T>(string jsonFile)
     {
-        List<T> listOfObjects = new List<T>();
+        List<T>? listOfObjects = new List<T>();
         try
         {
             using (StreamReader reader = new StreamReader(jsonFile))
@@ -81,28 +88,36 @@ public static class JsonHandler
         return listOfObjects;
     }
 
-    public static bool Remove<T>(T objectToRemove, string jsonFile)
+    public static bool Remove<T>(int removalID, string jsonFile) where T: ObjectHasID
     {
         List<T>? listOfObjects = Read<T>(jsonFile);
-        if (listOfObjects != null && listOfObjects.Contains(objectToRemove))
+        T? objectToRemove = Get<T>(removalID, jsonFile);
+        
+        if(objectToRemove != null)
         {
-            listOfObjects.Remove(objectToRemove);
-            bool writeSucces = Write<T>(listOfObjects, jsonFile);
-            return writeSucces;
+            if (listOfObjects != null && listOfObjects.Contains(objectToRemove))
+            {
+                listOfObjects.Remove(objectToRemove);
+                bool writeSucces = Write<T>(listOfObjects, jsonFile);
+                return writeSucces;
+            }
         }
         return false;
     }
 
-    public static T Get<T>(int objectID, string jsonFile)
+    public static T? Get<T>(int objectID, string jsonFile) where T : ObjectHasID
     {
-        List<T> listOfObjects = Read<T>(jsonFile);
-        foreach (T object in listOfObjects)
+        List<T>? listOfObjects = Read<T>(jsonFile);
+        if (listOfObjects != null)
         {
-            if (object.ID == objectID)
+            foreach (T item in listOfObjects)
             {
-                return object;
+                if (item.ID == objectID)
+                {
+                    return item;
+                }
             }
         }
-        return null;
+        return default;
     }
 }
