0e31b8cb8cc490fb59d7dd3b7d52a629a4626589
dabboloosefun <juliankreugel@gmail.com>
2024-03-22 01:24:30 +0100

    working on tests

--- CinemaReservationSystem/Movie.cs
+++ CinemaReservationSystem/Movie.cs
@@ -1,54 +1,47 @@
-public class Movie
+using CsvHelper.Configuration.Attributes;
+
+public class Movie : ObjectHasID
 {
-    public readonly int ID;
+    public int ID { get;}
     public string Title;
     public int AgeRating;
-    public List<int> ScreeningIDs = new List<int>;
-    public Movie(string title, int ageRating, List<int> screenings = null)
+    public List<int> ScreeningIDs = new List<int>();
+    public Movie(string title, int ageRating, List<int>? screenings = null)
     {
-        ID = (JsonHandler.Read<Movie>("MovieDB.json")).Count + 1;
+        List<Screening>? allMovies = JsonHandler.Read<Screening>("MovieDB.json");
+        ID = allMovies != null ? allMovies.Count + 1 : 1;
         Title = title;
         AgeRating = ageRating;
-        if (screenings != null) Screenings = screenings;
+        if (screenings != null) ScreeningIDs = screenings;
     }
 
     public void AddScreening(Auditorium assignedAuditorium, string screeningDateTime)
     {
-        Screenings newScreening = new Screening(assignedAuditorium, screeningDateTime, this.ID);
-        Screenings.Add(newScreening.ID);
-        JsonHandler.Update<Screening>(newScreening, "ScreeningDB.json");
-        JsonHandler.Update<Movie>(this, "MovieDB.json");
+        Screening newScreening = new Screening(assignedAuditorium, screeningDateTime, this.ID);
+        ScreeningIDs.Add(newScreening.ID);
+        bool updateSucces = JsonHandler.Update<Screening>(newScreening, "ScreeningDB.json");
+        updateSucces = JsonHandler.Update<Movie>(this, "MovieDB.json");
     }
 
     public void RemoveScreening(int screeningID)
     {
-        screeningToRemove = GetScreeningByID(screeningID);
-        JsonHandler.Remove<Screening>(screeningToRemove);
+        bool removalSucces = JsonHandler.Remove<Screening>(screeningID, "ScreeningDB.json");
     }
 
     public List<Screening> GetAllMovieScreenings()
     {
-        List<Screening> allScreenings = JsonHandler.Read<Screening>("ScreeningDB.json");
+        List<Screening>? allScreenings = JsonHandler.Read<Screening>("ScreeningDB.json");
         List<Screening> movieScreenings = new List<Screening>();
-        foreach (int screeningID in ScreeningIDs)
+        if (allScreenings != null)
         {
-            foreach (Screening screening in allScreenings)
+            foreach (int screeningID in ScreeningIDs)
             {
-                if (screening.ID == screeningID) movieScreenings.Add(screening);
+                foreach (Screening screening in allScreenings)
+                {
+                    if (screening.ID == screeningID) movieScreenings.Add(screening);
+                }
             }
         }
         return movieScreenings;
     }
-
-    public static Screening? GetScreeningByID(int screeningID)
-    {
-        List<Screening> allScreenings = JsonHandler.Read<Screening>("ScreeningDB.json");
-        foreach (Screening screening in allScreenings)
-        {
-            if (screening.ID == screeningID) return screening;
-        }
-        return null;
-    }
-
-
 }
