13482fca8b8fec65ed879b0d2b7ab5ef98303f43
ZeroTomato01 <146572680+ZeroTomato01@users.noreply.github.com>
2024-03-26 14:20:31 +0100

    Graphics and helper update to placement of methods and Readstring method added to helper

--- CinemaReservationSystem/Graphics.cs
+++ CinemaReservationSystem/Graphics.cs
@@ -2,9 +2,12 @@ public class Graphics
 {
     public static void BoxText(string text, string header = "")
     {
+
         string upperHeader = header.ToUpper();
         string upperText = text.ToUpper();
 
+        if (string.IsNullOrEmpty(upperText)) return;
+            
         string[] lines = upperText.Split('\n');
 
         int maxLineLength = lines.Max(line => line.Length);
@@ -28,121 +31,28 @@ public class Graphics
 
         if (!string.IsNullOrEmpty(upperHeader))
         {
-            WriteInCenter(middleBorder);
+            Helper.WriteInCenter(middleBorder);
         }
         else
         {
-            WriteInCenter("╔" + new string('═', totalWidth) + "╗");
+            Helper.WriteInCenter("╔" + new string('═', totalWidth) + "╗");
         }
 
         foreach (string line in lines)
         {
             leftPadding = (totalWidth - line.Length - 2) / 2;
-            WriteInCenter($"║ {new string(' ', leftPadding)}{line}{new string(' ', totalWidth - line.Length - leftPadding - 2)} ║");
+            Helper.WriteInCenter($"║ {new string(' ', leftPadding)}{line}{new string(' ', totalWidth - line.Length - leftPadding - 2)} ║");
         }
 
-        WriteInCenter(bottomBorder);
+        Helper.WriteInCenter(bottomBorder);
     }
 
-        public void Map()
+    public void AuditoriumView(string Auditorium)
     {
         Console.Clear();
-        string auditorium = $@"
-        ";
         int indexPos = 0;
-        switch (ID)
-        {
-            case World.LOCATION_ID_HOME:
-                indexPos = map.IndexOf("    |  ** ≈");
-                break;
+        var newmap = Helper.ReplaceAt(Auditorium, indexPos + 4, Convert.ToChar("X"));
 
-            case World.LOCATION_ID_TOWN_SQUARE:
-                indexPos = map.IndexOf("  │   │█");
-                break;
-
-            case World.LOCATION_ID_ALCHEMIST_HUT:
-                indexPos = map.IndexOf("│ ▄███▄ ");
-                break;
-
-            case World.LOCATION_ID_ALCHEMISTS_GARDEN:
-                indexPos = map.IndexOf("   ...  ");
-                break;
-
-            case World.LOCATION_ID_BURROW:
-                indexPos = map.IndexOf("*** +  ");
-                break;
-
-            case World.LOCATION_ID_FARMHOUSE:
-                indexPos = map.IndexOf("===F___");
-                break;
-
-            case World.LOCATION_ID_FARM_FIELD:
-                indexPos = map.IndexOf("  =====       │");
-                break;
-
-            case World.LOCATION_ID_SHACK:
-                indexPos = map.IndexOf("     ▐      └");
-                break;
-
-            case World.LOCATION_ID_TOMB2:
-                indexPos = map.IndexOf("   <=>        │");
-                break;
-
-            case World.LOCATION_ID_FIELD_SOUTH:
-                indexPos = map.IndexOf("      ▄▌ *");
-                break;
-
-            case World.LOCATION_ID_GUARD_POST:
-                indexPos = map.IndexOf(" │---G╠");
-                break;
-
-            case World.LOCATION_ID_TOMB:
-                indexPos = map.IndexOf("   <=>≈");
-                break;
-
-            case World.LOCATION_ID_BRIDGE:
-                indexPos = map.IndexOf("--G╠═B═");
-                break;
-
-            case World.LOCATION_ID_SPIDER_FIELD:
-                indexPos = map.IndexOf(" **S***");
-                break;
-        }
-        var newmap = ReplaceAt(map, indexPos + 4, Convert.ToChar("X"));
-
-        WriteLineWithColoredLetter(newmap);
-    }
-
-    public static void WriteLineWithColoredLetter(string letters)
-    {
-        Char[] array = letters.ToCharArray();
-        char[] yellow = {'S', 'B', 'G', 'P', 'A', 'V', 'F', 'T', 'H', '1', '2', '3', '4', '5' };
-
-        foreach (Char c in array)
-        {
-            if (c == 'X')
-            {
-                Console.ForegroundColor = System.ConsoleColor.DarkRed;
-                Console.Write(c);
-            }
-            else if (yellow.Contains(c))
-            {
-                Console.ForegroundColor = System.ConsoleColor.Yellow;
-                Console.Write(c);
-            }
-            else
-            {
-                Console.ForegroundColor = System.ConsoleColor.White;
-                Console.Write(c);
-            }
-        }
-        Console.WriteLine();
-    }
-
-    public static string ReplaceAt(string input, int index, char newChar)
-    {
-        char[] chars = input.ToCharArray();
-        chars[index] = newChar;
-        return new string(chars);
+        Helper.WriteColoredLetter(newmap);
     }
 }
--- CinemaReservationSystem/Helper.cs
+++ CinemaReservationSystem/Helper.cs
@@ -1,58 +1,159 @@
 public class Helper
+{
+    public static char ReadInput(Func<char, bool> validationCriteria, string reason="")
     {
-        public static char ReadInput(Func<char, bool> validationCriteria)
+        Graphics.BoxText(reason);
+        /* validate input examples for char
+                        char letterInput = ReadInput(char.IsLetter);
+
+        Validate input to be a digit
+        char digitInput = ReadInput(char.IsDigit);
+
+        Validate input to be a specific letter 'X'
+        char specificLetterInput = ReadInput((char c) => c == 'X');
+
+        Validate input to be a specific number '5'
+        char specificNumberInput = ReadInput((char c) => c == '5');*/
+        Console.CursorVisible = false;
+        char input;
+        bool isValidInput = false;
+        Console.WriteLine();
+        do
         {
-            /* validate input examples
-                          char letterInput = ReadInput(char.IsLetter);
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            input = keyInfo.KeyChar;
 
-            Validate input to be a digit
-            char digitInput = ReadInput(char.IsDigit);
+            isValidInput = validationCriteria(input);
 
-            Validate input to be a specific letter 'X'
-            char specificLetterInput = ReadInput((char c) => c == 'X');
-
-            Validate input to be a specific number '5'
-            char specificNumberInput = ReadInput((char c) => c == '5');*/
-            Console.CursorVisible = false;
-            char input;
-            bool isValidInput = false;
-
-            do
+            if (!isValidInput)
             {
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                input = keyInfo.KeyChar;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                WriteInCenter("Invalid input. Please try again.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            ClearLineDo();
+        } while (!isValidInput);
 
-                // Check if the input satisfies the validation criteria
-                isValidInput = validationCriteria(input);
+        Console.Clear();
+        return input;
+    }
 
-                if (!isValidInput)
-                {
-                    WriteInCenter("Invalid input. Please try again.");
-                }
+    public static string ReadInput(Func<string, bool> stringValidationCriteria)
+    {
+        /*
+            // Validate input to be a string that starts with 'ABC'
+        string abcInput = ReadInput(s => s.StartsWith("ABC"));
+
+        // Validate input to be a string that contains only digits
+        string digitsInput = ReadInput(s => s.All(char.IsDigit));
+        
+            */
+        Console.CursorVisible = false;
+        string input = string.Empty;
+        int windowWidth = Console.WindowWidth;
+        Console.WriteLine("\n\n");
+        do
+        {
+            int leftPadding = (windowWidth - input.Length) / 2;
+            Console.SetCursorPosition(leftPadding, Console.CursorTop);
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            ClearLineDo();
+
+            if (input.Length == 10)
+            {
+                ClearLastLine();
                 ClearLineDo();
-            } while (!isValidInput);
-
-            return input;
-        }
-        public static void ClearLastLine()
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write(new string(' ', Console.BufferWidth));
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-        }
-
-        public static void ClearLineDo()
-        {
-            Console.Write(new string(' ', Console.BufferWidth));
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-
-        }
-        public static void WriteInCenter(string data)
-        {
-            foreach (var model in data.Split('\n'))
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                WriteInCenter("Length exceeded. Please try again.");
+                Console.ForegroundColor = ConsoleColor.White;
+                input = string.Empty;
+            }
+            else if (keyInfo.Key == ConsoleKey.Backspace && input.Length > 0)
             {
-                Console.SetCursorPosition((Console.WindowWidth - model.Length) / 2, Console.CursorTop);
-                Console.WriteLine(model);
+                ClearLineDo();
+                input = input.Substring(0, input.Length - 1);
+                ClearLineDo();
+            }
+            else if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                if (!stringValidationCriteria(input) || string.IsNullOrEmpty(input))
+                {
+                    ClearLastLine();
+                    ClearLineDo();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    WriteInCenter("Invalid input. Please try again...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    input = string.Empty;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                ClearLastLine();
+                input += keyInfo.KeyChar;
+            }
+            Console.WriteLine();
+            WriteInCenter(input);
+        } while (true);
+
+        ClearLastLine();
+        return input;
+    }
+
+    public static void ClearLastLine()
+    {
+        Console.SetCursorPosition(0, Console.CursorTop - 1);
+        Console.Write(new string(' ', Console.BufferWidth));
+        Console.SetCursorPosition(0, Console.CursorTop - 1);
+    }
+
+    public static void ClearLineDo()
+    {
+        Console.Write(new string(' ', Console.BufferWidth));
+        Console.SetCursorPosition(0, Console.CursorTop - 1);
+
+    }
+    public static void WriteInCenter(string data)
+    {
+        foreach (var model in data.Split('\n'))
+        {
+            Console.SetCursorPosition((Console.WindowWidth - model.Length) / 2, Console.CursorTop);
+            Console.WriteLine(model);
+        }
+    }
+    public static void WriteColoredLetter(string letters)
+    {
+        Char[] array = letters.ToCharArray();
+        char[] yellow = { 'S', 'B', 'G', 'P', 'A', 'V', 'F', 'T', 'H', '1', '2', '3', '4', '5' };
+
+        foreach (Char c in array)
+        {
+            if (c == 'X')
+            {
+                Console.ForegroundColor = System.ConsoleColor.DarkRed;
+                Console.Write(c);
+            }
+            else if (yellow.Contains(c))
+            {
+                Console.ForegroundColor = System.ConsoleColor.Yellow;
+                Console.Write(c);
+            }
+            else
+            {
+                Console.ForegroundColor = System.ConsoleColor.White;
+                Console.Write(c);
             }
         }
+        Console.WriteLine();
+    }
+    public static string ReplaceAt(string input, int index, char newChar)
+    {
+        char[] chars = input.ToCharArray();
+        chars[index] = newChar;
+        return new string(chars);
+    }
 }
