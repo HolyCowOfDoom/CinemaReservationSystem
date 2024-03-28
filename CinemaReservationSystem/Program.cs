c17b6e7298f7ef8c6a8aeee8026b592ff3288b11
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2024-03-28 11:59:50 +0100

    Filled db up with prop data

--- CinemaReservationSystem/Program.cs
+++ CinemaReservationSystem/Program.cs
@@ -1,24 +1,27 @@
 ﻿using System.Security.AccessControl;
 
-User Utku = new("Utku", "08-04-2000", "ut.ozyurt@gmail.com", "moo");
-User Utku2 = new("Utku2", "09-04-2000", "ut.ozyurt@gmail.com", "moo2");
-
-List<User> tempUserList = new();
-tempUserList = CsvHandler.Read<User>(Path.GetFullPath("UserDB.csv"));
-for(int i = 0; i < tempUserList.Count; i++){
-    Console.WriteLine(tempUserList[i].Name);
-}
-
-User? tempUser = CsvHandler.GetRecordWithValue<User>("UserDB.csv", "Name", "Utku");
-User? tempUser2 = CsvHandler.GetRecordWithValue<User>("UserDB.csv", "ID", 1);
-Console.WriteLine(tempUser.ID + "   " + tempUser.Name);
-Console.WriteLine(tempUser2.ID + "   " + tempUser2.Name);
-
-CsvHandler.UpdateRecordOfID<User>("UserDB.csv", tempUser.ID, new User(tempUser) {Name = "Bob!"});
-// foreach(User user in tempUserList){
-//     Console.WriteLine(user.Name);
+// List<User> tempUserList = new();
+// tempUserList = CsvHandler.Read<User>(Path.GetFullPath("UserDB.csv"));
+// for(int i = 0; i < tempUserList.Count; i++){
+//     Console.WriteLine(tempUserList[i].Name);
 // }
+
+// User? tempUser = CsvHandler.GetRecordWithValue<User>("UserDB.csv", "Name", "Utku");
+// User? tempUser2 = CsvHandler.GetRecordWithValue<User>("UserDB.csv", "ID", 1);
+// Console.WriteLine(tempUser.ID + "   " + tempUser.Name);
+// Console.WriteLine(tempUser2.ID + "   " + tempUser2.Name);
+
+// CsvHandler.UpdateRecordOfID<User>("UserDB.csv", tempUser.ID, new User(tempUser) {Name = "Bob!"});
+// // foreach(User user in tempUserList){
+// //     Console.WriteLine(user.Name);
+// // }
+
+// Interface.GeneralMenu();
+// Console.ReadLine(); //keeps external terminal open
+
+List<Movie> demoMovies = JsonHandler.Read<Movie>("MovieDB.json");
+
+List<User> demoUsers = CsvHandler.Read<User>("UserDB.csv");
 
 Interface.GeneralMenu();
 Console.ReadLine(); //keeps external terminal open
-
