6e570c1d0c28d19c48ac1f388c711106a1f67fd0
Aortus <thijs1506@gmail.com>
2024-04-02 15:02:30 +0200

    Added the IsValidInput to InterfaceController

--- CinemaReservationSystem/InterfaceController.cs
+++ CinemaReservationSystem/InterfaceController.cs
@@ -51,11 +51,10 @@ public class InterfaceController
     public static void RegisterUser()
     {
         Console.WriteLine("USER REGISTRATION\n-------------------------------------");
-        // passed naar een validator, kan ook nog in ander file als we willen dat dit alleen view is.
-        string username = GetValidInput("Username needs to be atleast 3 characters and not more than 20 characters.\nEnter username: ", IsValidUsername);
-        string birthDate = GetValidInput("Birthdate needs to be dd-MM-yyyy.\nEnter birthdate: ", IsValidBD);
-        string email = GetValidInput("An email address needs to include (@) and (.).\nEnter email: ", IsValidEmail);
-        string password = GetValidInput("Password needs to be atleast 6 characters long and have a digit in it.\nEnter password: ", IsValidPassword);
+        string username = Helper.GetValidInput("Username needs to be atleast 3 characters and not more than 20 characters.\nEnter username: ", Helper.IsValidUsername);
+        string birthDate = Helper.GetValidInput("Birthdate needs to be dd-MM-yyyy.\nEnter birthdate: ", Helper.IsValidBD);
+        string email = Helper.GetValidInput("An email address needs to include (@) and (.).\nEnter email: ", Helper.IsValidEmail);
+        string password = Helper.GetValidInput("Password needs to be atleast 6 characters long and have a digit in it.\nEnter password: ", Helper.IsValidPassword);
         Console.Clear();
         User user = new User(username, birthDate, email, password);
         Console.WriteLine("User registration successful!");
@@ -64,64 +63,6 @@ public class InterfaceController
         Console.WriteLine($"Email: {user.Email}");
         
         XToGoBack(user.ID);
-
-        static string GetValidInput(string prompt, Func<string, bool> validation)
-        {
-            string input;
-            do
-            {
-                Console.Write(prompt);
-                input = Console.ReadLine();
-            } while (!validation(input));
-
-            return input;
-        }
-
-        static bool IsValidUsername(string input) => !string.IsNullOrWhiteSpace(input) && input.Length >= 3 && input.Length < 21;
-        static bool IsValidEmail(string input) => !string.IsNullOrWhiteSpace(input) && input.Contains('@') && input.Contains('.') && input.Length < 31;
-
-        // static bool IsValidName(string input)
-        // {
-        //     if (string.IsNullOrWhiteSpace(input))
-        //     {
-        //         return false;
-        //     }
-        //     string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        //     foreach (string word in words){
-        //         if(word.Length < 2) return false;
-        //     }
-        //     return true;
-        // }
-
-        static bool IsValidPassword(string input)
-        {
-            // test op lege string of string onder de 6 chars.
-            if (!string.IsNullOrWhiteSpace(input) && input.Length >= 6)
-            {
-                foreach (char c in input)
-                {
-                    if (char.IsDigit(c))
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        static bool IsValidBD(string input)
-        {
-            string dateFormat = "dd-MM-yyyy";
-            if (DateTime.TryParseExact(input, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
     
     private static void XToGoBack(){
@@ -139,6 +80,7 @@ public class InterfaceController
             UserInterface.GeneralMenu(id);
         }
     }
+
     private static void XToGoAdmin(string id){
         Console.WriteLine("Press x to go back to the main menu");
         char specificLetterInput = Helper.ReadInput((char c) => c == 'x');
