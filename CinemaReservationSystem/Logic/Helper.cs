3f805debd46d921fcad04ff61e51e1acf9066207
Utku Özyurt <87371120+HolyCowOfDoom@users.noreply.github.com>
2024-06-14 09:42:43 +0200

    improved email input checls

--- CinemaReservationSystem/Logic/Helper.cs
+++ CinemaReservationSystem/Logic/Helper.cs
@@ -473,10 +473,14 @@ public class Helper
     }
     public static bool IsValidEmail(string input, string altFilePath = "")
     {
-        string invalidsymbols = ",~`!$%^&*()_-+={}[]|\\:;<>?/\"\'";
+        string invalidsymbolslocal = ",~`!$%^&*()+={}[]|\\:;<>?/\"\'"; //allow -_.
+        string invalidsymbolsdomain = ",~`!$%^&*()-_+={}[]|\\:;<>?/\"\'"; //check for 1 "." below
+        string[] parts = input.Split("@");
+
         if(input.Count(c => c == '@') > 1) return false;
-        if(input.Count(c => c == '.') > 1) return false;
-        foreach(char c in invalidsymbols) if(input.Contains(c)) return false;
+        if(parts[1].Count(c => c == '.') > 1) return false;
+        foreach(char c in invalidsymbolslocal) if(parts[0].Contains(c)) return false;
+        foreach(char c in invalidsymbolsdomain) if(parts[1].Contains(c)) return false;
 
         List<User> users;
         if(altFilePath == "") users = CsvHandler.Read<User>("Data/UserDB.csv");
